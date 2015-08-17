bea38738f39fc5610649f7ea2f8af7342c68d91e
shenglow <sheng_195@hotmail.com>
2015-08-17 22:45:20 +0800

    update get detail function

--- application/models/ContactModel.php
+++ application/models/ContactModel.php
@@ -58,45 +58,53 @@ class ContactModel extends CI_Model {
 	public function get_detail($stu_id)
 	{
 		$json = array("studata"=>array(),
-						"photo"=>"",
-						"facebook"=>array(
+						"email"=>array(),
+						"account"=>array(
 							array(
 								"uid"=>array(),
-								"location"=>array(0=>"",1=>""),
-								"work"=>array()
+								"facebook"=>array(),
+								"work"=>array(),
+								"education"=>array()
 							)
-						)					
+						)	
 			);
 			
 		$query = 'SELECT * FROM `studata` WHERE `stu_id` = "'.$_POST["stu_id"].'"';
 		$result = $this->db->query($query);
-	
-		$json["studata"]=array_values($result->unbuffered_row('array'));	
+		$json["studata"]=array_values($result->unbuffered_row('array'));
+
+		$query = 'SELECT `email` FROM `email` WHERE `stu_id` = "'.$_POST["stu_id"].'"';
+		$result = $this->db->query($query);
+		while($row=$result->unbuffered_row('array')){
+			array_push($json['email'], array_values($row));
+		}
 							
-		$query = 'SELECT `uid`,`fb_name` FROM `fb` WHERE `stu_id` = "'.$_POST["stu_id"].'"';
+		$query = 'SELECT `uid` FROM `link` WHERE `stu_id` = "'.$_POST["stu_id"].'"';
 		$result = $this->db->query($query);
 		$i=0;
 		while($row=$result->unbuffered_row('array')){
-			$json["facebook"][$i]["uid"] = array_values($row); //convert associative array to index array --$array = array_values($array);
+			$json["account"][$i]["uid"] = array_values($row); //convert associative array to index array --$array = array_values($array);
 			$i++;
-		}		
-		for($j=0;$j<$i;$j++){	
-							
-			$query = 'SELECT `current_location`,`hometown_location` FROM `fb` WHERE `uid` = "'.$json["facebook"][$j]["uid"][0].'"';
+		}
+		
+		for($j=0;$j<$i;$j++){
+			$query = 'SELECT `fb_name`,`photo`,`current_location`,`hometown_location` FROM `fb` WHERE `uid` = "'.$json["account"][$j]["uid"][0].'"';
 			$result = $this->db->query($query);
 			while($row=$result->unbuffered_row('array')){
-				$json["facebook"][$j]["location"]= array_values($row); 
+				$json["account"][$j]["facebook"] = array_values($row);
 			}
-				
-			$query = 'SELECT * FROM `work` WHERE `uid` = "'.$json["facebook"][$j]["uid"][0].'"';
+			
+			$query = 'SELECT `employer`,`position`,`start_date`,`end_date` FROM `work` WHERE `uid` = "'.$json["account"][$j]["uid"][0].'"';
 			$result = $this->db->query($query);
 			while($row=$result->unbuffered_row('array')){
-				$json["facebook"][$j]["work"][]= array_values($row);
+				$json["account"][$j]["work"][] = array_values($row);
 			}
-				
-			$query = 'SELECT `photo` FROM `fb` WHERE `uid` = "'.$json["facebook"][$j]["uid"][0].'"';
+			
+			$query = 'SELECT `degree`,`school`,`year` FROM `education` WHERE `uid` = "'.$json["account"][$j]["uid"][0].'"';
 			$result = $this->db->query($query);
-			$json["photo"] = array_values($result->unbuffered_row('array'));
+			while($row=$result->unbuffered_row('array')){
+				$json["account"][$j]["education"][] = array_values($row);
+			}
 		}
 		
 		return $json;

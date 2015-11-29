b7ea4a7a32d2e112c610e6b6504afdbc90959e03
shenglow <sheng_195@hotmail.com>
2015-11-29 15:12:52 +0800

    add ci-phpunit-test for unit testing

---
+++ application/tests/models/ContactModel_test.php
@@ -0,0 +1,26 @@
+<?php
+
+class ContactModel_test extends TestCase
+{
+	public function setUp()
+	{
+		$this->resetInstance();
+		$this->CI->load->model('ContactModel');
+		$this->obj = $this->CI->ContactModel;
+	}
+	
+	public function test_get_permit()
+	{
+		$id = '10203510741249410';
+		$actual = $this->obj->get_permit($id);
+		$expected = true;
+		$this->assertEquals($expected, $actual);
+		
+		$id = '10203510741249420';
+		$actual = $this->obj->get_permit($id);
+		$expected = false;
+		$this->assertEquals($expected, $actual);
+	}
+}
+
+?>
